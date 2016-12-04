cf281c01def5f5b8a2e525afd4a06bd7a037b4fc
Wini Ya-on <wnyaonjr@gmail.com>
2016-12-04 22:39:01 +0800

    move, rename and copy

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,7 +1,12 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileOwnerAttributeView;
 import java.nio.file.attribute.FileTime;
@@ -15,9 +20,13 @@ public class FileManager {
 	static SimpleDateFormat dateFormatter = null;
 	static final String COMMAND_DIR = "dir";
 	static final String COMMAND_EXIT = "exit";
+	static final String COMMAND_DEL = "del";
+	static final String COMMAND_RENAME = "rename";
+	static final String COMMAND_MOVE = "move";
+	static final String COMMAND_COPY = "copy";
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		dateFormatter = new SimpleDateFormat("MM/dd/yyyy HH:mma");
 		String command = "";
@@ -27,20 +36,117 @@ public class FileManager {
 		do
 		{
 			System.out.print(currDir+"> ");
-			//TODO get current directory lWocation
+			//TODO get current directory location
 			 command = inputReader.nextLine();
-			 if(command.equals(COMMAND_DIR)) {
+			 
+			 String commandParts[] = command.split(" ");
+			 int commandLength = commandParts.length;
+			 
+			 if(commandParts[0].equals(COMMAND_DIR)) {
 				 displayFilesAndFolder(currDir);
-			 } else if (command.equals(COMMAND_EXIT)) {
+			 } else if (commandParts[0].equals(COMMAND_EXIT)) {
 				
-			 } else {
-				 System.out.println("command not supported.");
+			 } else if (commandParts[0].equals(COMMAND_DEL)) {
+				 
+				 if(commandLength > 1){
+					 try 
+					 {		Files.delete(Paths.get(currDir+"\\" + commandParts[1]));
+							System.out.println("Sucessful Delete: "+ commandParts[1]);
+					 
+					 } catch (IOException e) {
+						e.printStackTrace();
+					 }
+				 }
+				 else{
+					 	System.out.println("del <targetFile>");
+				 }
+				 
+			 }else if(commandParts[0].equals(COMMAND_RENAME)){
+				 
+				 if(commandLength > 1){
+					 
+					 File oldFileName = new File(currDir + "\\" + commandParts[1]);
+					 File newFileName = new File(currDir + "\\" + commandParts[2]);
+					  
+					 if (oldFileName.renameTo(newFileName)) {
+						  System.out.println("File renamed successfull !");
+					 } else {
+						  System.out.println("File rename operation failed !");
+					 }
+				 }
+				 else{
+					 	System.out.println("rename <oldFile>.<extension> <newFile>.<extension>");
+				 }
+				 	 
+			 }else if(commandParts[0].equals(COMMAND_MOVE)){
+				 
+				    InputStream inStream = null;
+					OutputStream outStream = null;
+					
+					File oldFile = new File(commandParts[1]);
+			    	File newFile = new File(commandParts[2]);
+
+			    	    inStream = new FileInputStream(oldFile);
+			    	    outStream = new FileOutputStream(newFile);
+
+			    	    byte[] buffer = new byte[1024];
+
+			    	    int length;
+			    	    //copy the file content in bytes
+			    	    while ((length = inStream.read(buffer)) > 0){
+
+			    	    	outStream.write(buffer, 0, length);
+
+			    	    }
+
+			    	    inStream.close();
+			    	    outStream.close();
+
+			    	    //delete the original file
+			    	    oldFile.delete();
+
+			    	    System.out.println("File is moved successful!");
+				 
+			 }
+			 else if(commandParts[0].equals(COMMAND_COPY)){
+				 
+				    InputStream inStream = null;
+					OutputStream outStream = null;
+					
+					File oldFile = new File(commandParts[1]);
+			    	File newFile = new File(commandParts[2]);
+
+			    	    inStream = new FileInputStream(oldFile);
+			    	    outStream = new FileOutputStream(newFile);
+
+			    	    byte[] buffer = new byte[1024];
+
+			    	    int length;
+			    	    //copy the file content in bytes
+			    	    while ((length = inStream.read(buffer)) > 0){
+
+			    	    	outStream.write(buffer, 0, length);
+
+			    	    }
+
+			    	    inStream.close();
+			    	    outStream.close();
+
+
+			    	    System.out.println("File is copied successful!");
+				 
+			 }
+			 else {
+				 if(!command.isEmpty()){
+					 System.out.println("command not supported.");
+				 }
 			 }
 			
 		}while(!command.equals(COMMAND_EXIT));
 		
-		inputReader.close();
+			inputReader.close();
 	}
+	
 	
 	/**
 	 * print files and directories inside a given path
@@ -62,8 +168,8 @@ public class FileManager {
 	    	System.out.println(getFileDetails(listOfFiles[i]));
 	    }
 	    
-	    System.out.println("Total Space: "+readableFileSize(totalSpace));
-	    System.out.println("Free Space: "+readableFileSize(freeSpace));
+	    System.out.println("Total Space: "+ readableFileSize(totalSpace));
+	    System.out.println("Free Space: "+ readableFileSize(freeSpace));
 	}
 	
 	private static String getFileDetails(File inputFile)
